c03fd52ac841f644db259b9bdcc151063bc347b9
ToshihiroOgino <tonigo0725@gmail.com>
2026-08-16 16:07:11 +0900

    Update version number to 1.0.0 in UserScript and simplify change history in README

--- clean_youtube_recommendations/clean-youtube-recommendations.user.js
+++ clean_youtube_recommendations/clean-youtube-recommendations.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Clean YouTube Recommendations
 // @namespace    https://github.com/ToshihiroOgino/custom_scripts
-// @version      1.2.1
+// @version      1.0.0
 // @description  YouTube の視聴ページで、再生回数が少ない動画を関連動画欄から取り除く
 // @author       ToshihiroOgino
 // @match        https://www.youtube.com/*
